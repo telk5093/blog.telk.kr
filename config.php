9ff9a71bab78a9446d44f940728ef36e7f7221e9
TELK <telk5093@gmail.com>
2021-12-19 15:01:25 +0900

    Add: Github corner

--- config.php
+++ config.php
@@ -1,12 +1,15 @@
 <?php
     $config = [
-        'siteTitle' => 'TELKblog',
-        'author'    => 'TELK',
-        'description' => 'TELK의 블로그',
-        'baseurl'     => 'https://blog.telk.kr',
+        'siteTitle'           => 'TELKblog',
+        'author'              => 'TELK',
+        'description'         => 'TELK의 블로그',
+        'baseurl'             => 'https://blog.telk.kr',
 
-        // 목록에서 미리 보여줄 본문의 글 제한
-        'content_limit' => 200,
+        // Character limit of contents
+        'content_limit'       => 200,
+
+        // Github
+        'github'              => 'https://github.com/telk5093',
 
         'google_analytics'    => 'UA-199820069-1',
         'google_analytics_v4' => 'G-ESB087YT8T',
