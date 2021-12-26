6134f711c5ba5727abb6b6ad3f8ea8dcc0e3ae14
TELK <telk5093@gmail.com>
2021-12-26 13:23:01 +0900

    Remove: Legacy blog codes

--- includes/list.php
+++
@@ -1,7 +0,0 @@
-	<li>
-		<a href="<?=$_postData['date']?>/<?=$_postData['uname']?>">
-            <time datetime="<?=$_postTime?>"><?=$_postData['date']?></time>
-            <h2><?=$_postData['meta']['title']?></h2>
-            <p><?=$_listContent?></p>
-        </a>
-	</li>
